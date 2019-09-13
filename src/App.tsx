675f56e9fdcb2db2725623c2ce7b7909335ada78
Manu Rana <manu.rana@baxi.taxi>
2019-09-13 18:04:02 +0530

    ionic components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from "react";
-import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
+import { IonApp, IonPage } from "@ionic/react";
 import "./App.css";
+import Signin from "./pages/Signin/Signin";
+import * as ROUTES from "./constants/routes";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <IonApp>
+        <IonPage>
+          <Switch>
+            {/* <Route exact path={ROUTES.SIGN_IN} component={Signin} /> */}
+            <Route exact path="/" component={Signin} />
+          </Switch>
+        </IonPage>
+      </IonApp>
+    </Router>
   );
 };
 
